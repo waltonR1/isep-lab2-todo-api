4d2dfb3e081442ee3b38bd4544cf141811971d09
waltonR1 <waltonR1@163.com>
2025-05-13 02:45:29 +0200

    step 5

--- src/main/java/org/isep/cleancode/Main.java
+++ src/main/java/org/isep/cleancode/Main.java
@@ -2,15 +2,46 @@ package org.isep.cleancode;
 
 import static spark.Spark.*;
 
+import org.isep.cleancode.application.ITodoRepository;
 import org.isep.cleancode.persistence.csvfiles.TodoCsvFilesRepository;
+import org.isep.cleancode.persistence.inmemory.TodoInMemoryRepository;
 import org.isep.cleancode.presentation.TodoController;
 
-public class Main {
-    static TodoCsvFilesRepository repo = new TodoCsvFilesRepository();
-    private static final TodoController todoController = new TodoController(repo);
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
+public class Main {
     public static void main(String[] args) {
         port(4567);
+
+        String repoType = "INMEMORY";
+
+        try (FileInputStream fis = new FileInputStream("config.properties")) {
+            Properties props = new Properties();
+            props.load(fis);
+            String configValue = props.getProperty("repo");
+            if (configValue != null) {
+                repoType = configValue.toUpperCase();
+            }
+        } catch (IOException e) {
+            System.out.println("Can't read config.properties, using default configuration INMEMORY");
+        }
+
+        ITodoRepository repo;
+        switch (repoType) {
+            case "CSV":
+                System.out.println("Using CSV file repository.");
+                repo = new TodoCsvFilesRepository();
+                break;
+            case "INMEMORY":
+            default:
+                System.out.println("Using in-memory repository.");
+                repo = new TodoInMemoryRepository();
+                break;
+        }
+
+        TodoController todoController = new TodoController(repo);
 
         get("/todos", todoController::getAllTodos);
 
